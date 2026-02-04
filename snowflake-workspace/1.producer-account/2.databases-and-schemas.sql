242133575c5344941e0b2ffd36cf806ad5c5848b
rahulrajpr <rahul.infobox@gmail.com>
2026-02-04 16:34:24 +0530

    created schema for landing and dbt devs

--- snowflake-workspace/1.producer-account/2.databases-and-schemas.sql
+++ snowflake-workspace/1.producer-account/2.databases-and-schemas.sql
@@ -16,9 +16,29 @@ USE DATABASE FINANCE_DB;
 
 SHOW SCHEMAS;
 
+---------------------------------------------------------
+--> SCHEMA FOR RAW DATA FROM POSTGRES AND APIS (IN GCP)
+---------------------------------------------------------
+
 CREATE SCHEMA IF NOT EXISTS FINANCE_DB.POSTGRES_RAW;
 
 CREATE SCHEMA IF NOT EXISTS FINANCE_DB.GCS_RAW;
+
+--------------------------------------------------------------
+--> UNIFIED SCHEMA FOR ALL RAW TABLES WITH MIN TRANSFORMATION
+-------------------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS FINANCE_DB.LANDING;
+
+--------------------------------------------------------------
+--> SCHEMA , USED DBT DEVELOPMENT
+-------------------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS FINANCE_DB.STAGING;
+
+CREATE SCHEMA IF NOT EXISTS FINANCE_DB.INTERMEDIATE;
+
+CREATE SCHEMA IF NOT EXISTS FINANCE_DB.ANALYTICS;
 
 --===============================
 -- VERIFY THE SAME
@@ -31,4 +51,3 @@ SHOW SCHEMAS
 --===============================
 -- END OF DATABASES AND SCHEMAS
 --===============================
-
