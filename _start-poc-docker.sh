c1ef81246a1d4d096c6f6f6a2c1de98523f70fca
rahulrajpr <rahul.infobox@gmail.com>
2026-02-05 21:18:13 +0530

    updated the start poc docker file

--- _start-poc-docker.sh
+++ _start-poc-docker.sh
@@ -6,7 +6,7 @@ echo "Starting Data POC Platform..."
 cd docker
 
 # 1. Start core stack
-echo "Starting PostgreSQL, Airflow, Superset, DBT, Meltano..."
+echo "Starting PostgreSQL, Trino, Airflow, Superset, DBT..."
 docker compose up -d
 
 # 2. Back to the Root Directory
