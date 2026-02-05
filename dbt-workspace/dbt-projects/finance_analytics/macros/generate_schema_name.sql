ea600d0752542c64eca60fcf05fda0fdb8cc09bf
rahulrajpr <rahul.infobox@gmail.com>
2026-02-05 11:54:41 +0530

    macro added for the current schema mapping

---
+++ dbt-workspace/dbt-projects/finance_analytics/macros/generate_schema_name.sql
@@ -0,0 +1,13 @@
+
+-- This macro is very important for the proper schema mapping
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+    {%- set default_schema = target.schema -%}
+    
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+
+{%- endmacro %}
